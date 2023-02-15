2deeecce30f572355f4735c133417604c503021f
Van-Giang NGUYEN <van.giang.nguyen@outlook.com.vn>
2023-02-15 11:01:19 +0700

    init

---
+++ overload.js
@@ -0,0 +1,8 @@
+//Function Definition
+function add(a, b) {
+    return a + b;
+}
+//Result
+console.log("Addition: " + add("Hello ", "JavaTpoint"));
+console.log("Addition: " + add(30, 20));
+console.log("Addition: "+add(30, "20"));
